94ed974acd1f061d4c585bfb4be4d19816ff610d
asanfilov <25196996+asanfilov@users.noreply.github.com>
2022-05-27 20:37:37 -0300

    Add a test for 'Switch to alert' exercise
    To run the test: dotnet test --filter SwitchToAlert

---
+++ src/SeleniumTraining/FormyTest1.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace SeleniumTraining
+{
+    public class FormyTest1
+    {
+        [Fact]
+        [Trait("Exercise", "https://www.linkedin.com/learning/selenium-essential-training/switch-to-alert")]
+        public void SwitchToAlert()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://formy-project.herokuapp.com/switch-window");
+            IWebElement openAlert = driver.FindElement(By.Id("alert-button"));
+            System.Threading.Thread.Sleep(1_000);
+            openAlert.Click();
+
+            System.Threading.Thread.Sleep(2_000);//add a pause to see what the test does
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.Accept();
+
+            System.Threading.Thread.Sleep(2_000);
+            driver.Quit();
+        }
+    }
+}
