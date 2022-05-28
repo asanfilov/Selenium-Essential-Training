eb2e463c475d4eec2c0c19bfcb7a2e447ce186b3
asanfilov <25196996+asanfilov@users.noreply.github.com>
2022-05-28 13:11:57 -0300

    Rename FormyTest1 class and refactor: implement Constructor and Dispose (shared setup/cleanup code without sharing object instances)
    xUnit.net creates a new instance of the test class for every test that is run, so any code which is placed into the constructor of the test class will be run for every single test.
    See https://xunit.net/docs/shared-context
    Note: if a test encounters an exception, Dispose() method will quit the browser driver.

--- src/SeleniumTraining/FormyTests.cs
+++ src/SeleniumTraining/FormyTests.cs
@@ -1,17 +1,29 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Xunit;
 
 namespace SeleniumTraining
 {
-    public class FormyTest1
+    public class FormyTests : IDisposable
     {
+        private readonly IWebDriver driver;
+
+        public FormyTests()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            System.Threading.Thread.Sleep(2_000);
+            driver.Quit();
+        }
+
         [Fact]
         [Trait("Exercise", "Shows how to click a button in alert https://www.linkedin.com/learning/selenium-essential-training/switch-to-alert")]
         public void SwitchToAlert()
         {
-            IWebDriver driver = new ChromeDriver();
-
             driver.Navigate().GoToUrl("https://formy-project.herokuapp.com/switch-window");
             IWebElement openAlert = driver.FindElement(By.Id("alert-button"));
             System.Threading.Thread.Sleep(1_000);
@@ -20,16 +32,12 @@ namespace SeleniumTraining
             System.Threading.Thread.Sleep(2_000);//add a pause to see what the test does
             IAlert alert = driver.SwitchTo().Alert();
             alert.Accept();
-
-            System.Threading.Thread.Sleep(2_000);
-            driver.Quit();
         }
 
         [Fact]
         [Trait("Exercise", "Shows how to close a modal https://www.linkedin.com/learning/selenium-essential-training/executing-javascript-commands")]
         public void ExecuteJavaScript()
         {
-            IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://formy-project.herokuapp.com/modal");
 
             IWebElement modalButton = driver.FindElement(By.Id("modal-button"));
@@ -37,12 +45,9 @@ namespace SeleniumTraining
             modalButton.Click();
 
             IWebElement closeButton = driver.FindElement(By.Id("close-button"));
-            //Note: closeButton.Click(); will throw OpenQA.Selenium.ElementNotInteractableException
+            //closeButton.Click(); //will throw OpenQA.Selenium.ElementNotInteractableException
             IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
             js.ExecuteScript("arguments[0].click();", closeButton);
-
-            System.Threading.Thread.Sleep(2_000);
-            driver.Quit();
         }
     }
 }
