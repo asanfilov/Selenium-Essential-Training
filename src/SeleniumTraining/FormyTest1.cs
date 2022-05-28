1b1dda0cdf9ff732a463df08335ec101df63e1be
asanfilov <25196996+asanfilov@users.noreply.github.com>
2022-05-28 12:54:14 -0300

    Add a test for 'Executing JavaScript commands' exercise that shows how to close a modal

--- src/SeleniumTraining/FormyTest1.cs
+++ src/SeleniumTraining/FormyTest1.cs
@@ -7,7 +7,7 @@ namespace SeleniumTraining
     public class FormyTest1
     {
         [Fact]
-        [Trait("Exercise", "https://www.linkedin.com/learning/selenium-essential-training/switch-to-alert")]
+        [Trait("Exercise", "Shows how to click a button in alert https://www.linkedin.com/learning/selenium-essential-training/switch-to-alert")]
         public void SwitchToAlert()
         {
             IWebDriver driver = new ChromeDriver();
@@ -20,6 +20,26 @@ namespace SeleniumTraining
             System.Threading.Thread.Sleep(2_000);//add a pause to see what the test does
             IAlert alert = driver.SwitchTo().Alert();
             alert.Accept();
+
+            System.Threading.Thread.Sleep(2_000);
+            driver.Quit();
+        }
+
+        [Fact]
+        [Trait("Exercise", "Shows how to close a modal https://www.linkedin.com/learning/selenium-essential-training/executing-javascript-commands")]
+        public void ExecuteJavaScript()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://formy-project.herokuapp.com/modal");
+
+            IWebElement modalButton = driver.FindElement(By.Id("modal-button"));
+            System.Threading.Thread.Sleep(1_000);
+            modalButton.Click();
+
+            IWebElement closeButton = driver.FindElement(By.Id("close-button"));
+            //Note: closeButton.Click(); will throw OpenQA.Selenium.ElementNotInteractableException
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].click();", closeButton);
 
             System.Threading.Thread.Sleep(2_000);
             driver.Quit();
