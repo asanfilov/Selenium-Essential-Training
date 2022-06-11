247c350da142a7f89f01a3a5a6d0825dd71b7f07
asanfilov <25196996+asanfilov@users.noreply.github.com>
2022-06-10 22:39:54 -0300

    Define Synchole URLs as constants

---
+++ src/SeleniumTraining/SynchronizationStrategies/SyncholePages.cs
@@ -0,0 +1,10 @@
+﻿namespace SeleniumTraining.SynchronizationStrategies
+{
+    internal class SyncholePages
+    {
+        public const string Collapseable = "https://eviltester.github.io/synchole/collapseable.html";
+        public const string Buttons = "https://eviltester.github.io/synchole/buttons.html";
+        public const string Form = "https://eviltester.github.io/synchole/form.html";
+        public const string Messages = "https://eviltester.github.io/synchole/messages.html";
+    }
+}
